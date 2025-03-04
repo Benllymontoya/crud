ee77053f5ceee802b1b0d025d0dd106fbbf343d5
benllymontoya <benllymontoya02@gmail.com>
2025-03-03 22:14:24 -0500

    Segunda seccion productos hechos tabla bd

--- database/migrations/2025_03_04_023356_create_products_table.php
+++ database/migrations/2025_03_04_023356_create_products_table.php
@@ -11,9 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Elimina la tabla 'products' si existe
+        Schema::dropIfExists('products');
+        
+        // Crea una nueva tabla llamada 'products'
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('id'); // Añade una columna 'id' que será la clave primaria y se auto-incrementará
+            $table->string('nombre'); // Añade una columna 'nombre' de tipo string
+            $table->text('descripcion')->nullable(); // Añade una columna 'descripcion' de tipo texto que puede ser nula
+            $table->decimal('precio', 8, 2); // Añade una columna 'precio' de tipo decimal con 8 dígitos en total y 2 decimales
+            $table->integer('stock'); // Añade una columna 'stock' de tipo entero
+            $table->timestamps(); // Añade columnas 'created_at' y 'updated_at' para registrar las marcas de tiempo de creación y actualización
         });
     }
 
@@ -22,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('products'); // Elimina la tabla 'products' si existe
     }
 };
